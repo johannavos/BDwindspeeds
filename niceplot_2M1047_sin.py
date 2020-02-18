e6fec695306e083d1847450a04919511e571047f
johannavos <43185024+johannavos@users.noreply.github.com>
2020-02-18 10:48:12 +0000

    Add files via upload

--- niceplot_2M1047_sin.py
+++ niceplot_2M1047_sin.py
@@ -7,20 +7,21 @@ import corner
 import IPython
 from astropy.io import fits
 
-hdulist = fits.open('2M1047_sin_flat_chain_ap3_bin5_epoch2.fits') #epoch 2
+#hdulist = fits.open('results/2M1047_sin_flat_chain_fourier_period_phaseoffset.fits')
+hdulist = fits.open('results/2M0147_sin_flat_chain_fourier_period__ap2_bin5.fits') #epoch 2
 
 samples = hdulist['PRIMARY'].data
                     
 samples[:, 0] = samples[:, 0] * 100.
 fig = corner.corner(samples,  labels=["$\mathrm{Amp }~(\%)$", "$~~~~~~\mathrm{  Period}~\mathrm{(hr)}$", "$\mathrm{Phase}$",
 	 "$\mathrm{Mean}$"], quantiles = [0.16, 0.5, 0.84], show_titles=True, label_kwargs={"fontsize":18},
-	 title_kwargs={"fontsize":15}, title_fmt=".3f",plot_datapoints=False, top_ticks = False,color="teal",
-	 quantiles_color="black",plot_contours=True,bins=100)
+	 title_kwargs={"fontsize":15}, title_fmt=".3f",plot_datapoints=False, 
+	 top_ticks = False,color="teal",quantiles_color="black",plot_contours=True,bins=100)
 for ax in fig.get_axes():
     ax.tick_params(axis='both', labelsize=12)
     ax.tick_params(axis="x", direction="inout")
     ax.tick_params(axis="y", direction="inout")
     ax.tick_params(bottom=True, top=True, left=True, right=True)
-fig.savefig("results/2M1047_tri_epoch2_pub.pdf",dpi=100 )
+fig.savefig("results/2M1047_tri_epoch1_pub.pdf",dpi=100 )
 plt.clf()
 
